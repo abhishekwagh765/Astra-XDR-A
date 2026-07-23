a627f94ef6ba49c377aad7a6498da7aa572b3ae9
mrrobot7781 <fcn7781@gmail.com>
2026-07-23 08:07:16 +0000

    semgrep checking

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import subprocess
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return jsonify({
-        "project": "ASTRA-XDR",
-        "status": "Running",
-        "phase": 1
+        "message": "Welcome to ASTRA-XDR",
+        "status": "Running"
     })
 
 @app.route("/health")
@@ -16,5 +16,18 @@ def health():
         "status": "Healthy"
     })
 
+# -------------------------------
+# TEST ENDPOINT (INTENTIONALLY VULNERABLE)
+# Used only to verify Semgrep CI
+# Remove after testing.
+# -------------------------------
+@app.route("/test")
+def test():
+    cmd = request.args.get("cmd")
+    subprocess.run(cmd, shell=True)
+    return jsonify({
+        "message": "Command executed"
+    })
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
